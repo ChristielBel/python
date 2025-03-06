3f188a4b1cab10662bb408bbf6d9e0d122e5dc72
Christina Belikova <119342375+ChristielBel@users.noreply.github.com>
2025-03-06 10:31:51 +0300

    Update postress.sql

--- postress.sql
+++ postress.sql
@@ -18,7 +18,8 @@ create table Futura(
 
 create table FuturaInfo(
 	id SERIAL PRIMARY KEY,
-	idFutura int REFERENCES Product(id),
+	idFutura int REFERENCES Futura(id),
+	idProduct int references Product(id),
 	quantity int not null,
 	price decimal(10,2) not null
 );
@@ -46,3 +47,19 @@ create trigger ins_futura_info after insert on FuturaInfo
 
 create trigger del_futura_info after delete on FuturaInfo
  for each row execute procedure delete_futura_info();
+
+
+insert into product (name, ed) values ('товар 1', 'шт');
+insert into product (name, ed) values ('товар 2', 'кг');
+insert into product (name, ed) values ('товар 3', 'л');
+
+insert into client (name, address, phone) values ('Клиент 1', 'Адрес 1', '+8324959345');
+insert into client (name, address, phone) values ('Клиент 2', 'Адрес 2', '+8356469345');
+insert into client (name, address, phone) values ('Клиент 3', 'Адрес 3', '+8324955445');
+
+insert into futura (idClient, data) values (1, '2023-10-01');
+insert into futura (idClient, data) values (2, '2023-10-08');
+
+insert into futurainfo (idFutura, idProduct, quantity, price) values (1,1,10,100);
+insert into futurainfo (idFutura, idProduct, quantity, price) values (1,2,5,200);
+insert into futurainfo (idFutura, idProduct, quantity, price) values (2,3,8,150);
